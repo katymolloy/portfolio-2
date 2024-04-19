11a8cbb5272a103f4e508442a67a979605ea941c
Katy Molloy <katyxmolloy@gmail.com>
2024-04-19 16:21:37 -0400

    almost finished

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 
-const observer = new IntersectionObserver((entries) => {
+const observer1 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
         }
     })
 })
 
+const observer2 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('showPill')
+        }
+    })
+})
+
+const openMenu = () =>{
+    let menuLinks = document.getElementById('nav-links');
+    console.log(menuLinks)
+    if(menuLinks.style.display === 'none'){
+        menuLinks.style.display === 'block'
+    }else{
+        menuLinks.style.display === 'none'
+    }
+}
+
 const hiddenCards = document.querySelectorAll('.hidden')
-hiddenCards.forEach((el) => observer.observe(el))
+hiddenCards.forEach((el) => observer1.observe(el))
+
+const hiddenSkillPills = document.querySelectorAll('.hiddenPill')
+hiddenSkillPills.forEach((el) => observer2.observe(el))
